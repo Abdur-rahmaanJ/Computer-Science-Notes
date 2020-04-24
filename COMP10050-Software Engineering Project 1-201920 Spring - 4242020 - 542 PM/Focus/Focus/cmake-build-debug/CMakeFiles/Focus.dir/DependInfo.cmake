0f38e08333887199bced04a5948eb5d1ef38f5c1
AbhishekSinhaCoder <abhisheksinha1506@gmail.com>
2020-04-24 18:15:27 +0100

    Added more.

---
+++ COMP10050-Software Engineering Project 1-201920 Spring - 4242020 - 542 PM/Focus/Focus/cmake-build-debug/CMakeFiles/Focus.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/liliana1/CLionProjects/Focus/game_init.c" "/Users/liliana1/CLionProjects/Focus/cmake-build-debug/CMakeFiles/Focus.dir/game_init.c.o"
+  "/Users/liliana1/CLionProjects/Focus/input_output.c" "/Users/liliana1/CLionProjects/Focus/cmake-build-debug/CMakeFiles/Focus.dir/input_output.c.o"
+  "/Users/liliana1/CLionProjects/Focus/main.c" "/Users/liliana1/CLionProjects/Focus/cmake-build-debug/CMakeFiles/Focus.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ COMP10050-Software Engineering Project 1-201920 Spring - 4242020 - 542 PM/Focus/Focus/cmake-build-debug/CMakeFiles/Focus.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Focus.dir/game_init.c.o"
+  "CMakeFiles/Focus.dir/input_output.c.o"
+  "CMakeFiles/Focus.dir/main.c.o"
+  "Focus"
+  "Focus.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Focus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
